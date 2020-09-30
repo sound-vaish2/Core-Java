3b6dc8b78790382d25754ce16cff71e87eb0ba23
sound-vaish2 <vaishnavisoundane@gmail.com>
2020-09-30 21:44:07 +0530

    Find duplicates in an array

---
+++ src/com/vs/java/Array/FindDuplicates.java
@@ -0,0 +1,23 @@
+package com.vs.java.Array;
+//Array
+public class FindDuplicates {
+
+	public FindDuplicates() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,3,4,5,6,7,7,8};
+		
+		for(int i=0;i<arr.length-1;i++)
+		{
+			if(arr[i] == arr[i+1])
+			{
+				System.out.println(arr[i]);
+			}
+		}
+		
+
+	}
+
+}
